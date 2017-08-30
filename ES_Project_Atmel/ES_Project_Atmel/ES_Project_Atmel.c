1fb462c4ad337e63d43c9c5fbfeb27b328bb1af8
Group41 <adrien.gerber@ensea.fr>
2017-08-30 09:23:50 +0200

    First try

---
+++ ES_Project_Atmel/ES_Project_Atmel/ES_Project_Atmel.c
@@ -0,0 +1,17 @@
+/*
+ * ES_Project_Atmel.c
+ *
+ * Created: 30.08.2017 09:16:45
+ *  Author: asgerber
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
